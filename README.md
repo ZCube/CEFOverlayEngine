e36ab1525c6d27210d8466498ef28ba68cfd9c92
ZCube <zcube@zcube.kr>
2017-07-04 22:44:18 +0900

    Update ScreenShot

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # CEFOverlayEngine
 
-##
+## Interface
+Button or Key | Description
+-------- | ----
+Shift + F3 | Show/Hide
+Ctrl + Alt + Shift | Click-through.
+
+## Screenshot
+![N|Solid](https://raw.githubusercontent.com/ZCube/CEFOverlayEngine/master/screenshot.png)
 
 NO WARRANTY. ANY USE OF THE SOFTWARE IS ENTIRELY AT YOUR OWN RISK.
--- dist.bat
+++ dist.bat
@@ -1,10 +1,10 @@
 mkdir dist\module\32
-xcopy /hrkys /exclude:exclude_files_2.txt bin\32\Release\loader.* dist\module\ffxiv_mod.*
+xcopy /hrkys /exclude:exclude_files_2.txt bin\32\Release\loader.* dist\module\loader32_mod.*
 xcopy /hrkys /exclude:exclude_files_2.txt bin\32\Release\CefOverlay.* dist\module\32\overlay_mod.*
 xcopy /hrkys external\reshade\bin\Win32\Release\ReShade32.dll dist\module\d3d9.*
 
 mkdir dist\module\64
-xcopy /hrkys /exclude:exclude_files_2.txt bin\64\Release\loader.* dist\module\ffxiv_dx11_mod.*
+xcopy /hrkys /exclude:exclude_files_2.txt bin\64\Release\loader.* dist\module\loader64_mod.*
 xcopy /hrkys /exclude:exclude_files_2.txt bin\64\Release\CefOverlay.* dist\module\64\overlay_mod.*
 xcopy /hrkys external\reshade\bin\x64\Release\ReShade64.dll dist\module\dxgi.*
 
@@ -19,13 +19,13 @@ xcopy /hrkys /exclude:exclude_files_2.txt prebuilt\cef\64\Release\* dist\runtime
 mkdir dist\with_runtime\32
 xcopy /hrkys /exclude:exclude_files_2.txt prebuilt\cef\32\Resources\* dist\with_runtime\32
 xcopy /hrkys /exclude:exclude_files_2.txt prebuilt\cef\32\Release\* dist\with_runtime\32
-xcopy /hrkys /exclude:exclude_files_2.txt bin\32\Release\loader.* dist\with_runtime\ffxiv_mod.*
+xcopy /hrkys /exclude:exclude_files_2.txt bin\32\Release\loader.* dist\with_runtime\loader32_mod.*
 xcopy /hrkys /exclude:exclude_files_2.txt bin\32\Release\CefOverlay.* dist\with_runtime\32\overlay_mod.*
 xcopy /hrkys external\reshade\bin\Win32\Release\ReShade32.dll dist\with_runtime\d3d9.*
 
 mkdir dist\with_runtime\64
 xcopy /hrkys /exclude:exclude_files_2.txt prebuilt\cef\64\Resources\* dist\with_runtime\64
 xcopy /hrkys /exclude:exclude_files_2.txt prebuilt\cef\64\Release\* dist\with_runtime\64
-xcopy /hrkys /exclude:exclude_files_2.txt bin\64\Release\loader.* dist\with_runtime\ffxiv_dx11_mod.*
+xcopy /hrkys /exclude:exclude_files_2.txt bin\64\Release\loader.* dist\with_runtime\loader64_mod.*
 xcopy /hrkys /exclude:exclude_files_2.txt bin\64\Release\CefOverlay.* dist\with_runtime\64\overlay_mod.*
 xcopy /hrkys external\reshade\bin\x64\Release\ReShade64.dll dist\with_runtime\dxgi.*
